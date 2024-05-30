d246154435a812371ce160abec31ae6323e9bb60
luigi.aceto <luigi.aceto@genre.com>
2024-05-30 16:37:29 +0200

    add script to download data from yf

---
+++ download yf data script gha.R
@@ -0,0 +1,35 @@
+library(yahoofinancer)
+
+tickers <- readRDS("tickers.rds")
+
+
+download_data <- function(ticker){
+  tryCatch(
+    {
+      Sys.sleep(5)
+      print(ticker)
+      data <- Ticker$new(ticker)
+      # data <- data$get_history(start = Sys.Date(), interval = '1d')
+      
+      data <- data$get_history(start = as.character(as.Date(Sys.Date() - 1)), interval = '1d')
+      data$ticker = ticker
+      data <- as.data.frame(subset(data, !is.na(volume)))
+      name_file <- paste0("./data/", ticker, ".txt")
+      write.table(x = data, file = name_file, sep = "\t", dec = ".")
+      # write.table(data, "./data/data.txt", append = TRUE, row.names = FALSE, col.names = FALSE) 
+      # Sys.Date()
+      # as.character(as.Date(Sys.Date() - 1))  
+    },
+    error=function(e) {
+      message('An Error Occurred')
+      print(e)
+    },
+    warning=function(w) {
+      message('A Warning Occurred')
+      print(w)
+      return(NA)
+    }
+  )
+}
+
+lapply(tickers, download_data)
