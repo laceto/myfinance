afc2e0cb49840239d2cfdfc2689aaa9e6b74d338
Luigi Aceto <luigi.aceto@genre.com>
2024-06-03 16:01:50 +0200

    Update script.R

--- script.R
+++ script.R
@@ -1,11 +1,5 @@
-install.packages("curl")
-# install.packages("httr")
-# install.packages("jsonlite")
-# install.packages("lubridate")
-# install.packages("magrittr")
-# install.packages("purrr")
-# install.packages("R6")
-# install.packages("stringr")
+#install.packages("curl")
+install.packages("openxlsx")
 install.packages("yahoofinancer", dependencies = TRUE)
 library(yahoofinancer)
 
@@ -32,6 +26,8 @@ download_data <- function(ticker){
       data$ticker = ticker
       print(data)
       #name_file <- paste0("./data/", ticker, ".txt")
+      name_file <- paste0("./data/", ticker, ".xlsx")
+      openxlsx::write.xlsx(data, file = name_file)
       #write.table(x = data, file = name_file, sep = "\t", dec = ".", row.names = FALSE)
       # write.table(data, "./data/data.txt", append = TRUE, row.names = FALSE, col.names = FALSE)
       # Sys.Date()
