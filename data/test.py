9e5fc97d2e4d8bccf0b00caa561b3cca9a7d8297
Luigi Aceto <luigi.aceto@genre.com>
2024-06-07 13:02:09 +0200

    Update test.py

--- data/test.py
+++ data/test.py
@@ -11,4 +11,4 @@ bm_df = read_xlsx(filename_bm)
 bm_name = filename_bm.replace('xlsx', '')
 
 files = [file for file in os.path.join(os.getcwd(), 'data') if file.endswith(".xlsx") and file != "FTSEMIB.MI.xlsx" and file != "marginabili.xlsx" and file != "sectors.xlsx" and file != "output_signals.xlsx" and file != "PTF.xlsx"]  
-print(bm_df.head())
+print(os.path.abspath())
