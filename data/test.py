6911c6e0dafb56b1c4b3ee7cf9b10c7fc7e8b22d
luigi.aceto <luigi.aceto@genre.com>
2024-07-15 10:23:48 +0200

    new code to read files

--- data/test.py
+++ data/test.py
@@ -10,12 +10,12 @@ filename_bm = 'data/FTSEMIB.MI.xlsx'
 bm_df = read_xlsx(filename_bm)
 bm_name = filename_bm.replace('xlsx', '')
 
-# files = [file for file in os.listdir('data') if file.endswith(".xlsx") and file != "FTSEMIB.MI.xlsx" and file != "marginabili.xlsx" and file != "sectors.xlsx" and file != "output_signals.xlsx" and file != "PTF.xlsx"]  
+# files = [file for file in os.listdir('data') if file.endswith(".xlsx") and file != "FTSEMIB.MI.xlsx" and file != "marginabili.xlsx" and file != "sectors.xlsx" and file != "output_signals.xlsx" and file != "PTF.xlsx", and file == "A2A.MI.xlsx"]
 # print(len(files))
 
 exclude_files = {"FTSEMIB.MI.xlsx", "marginabili.xlsx", "sectors.xlsx", "output_signals.xlsx", "PTF.xlsx"}
 include_files = {"A2A.MI.xlsx"}
-files = [file for file in os.listdir('/data') if file.endswith(".xlsx") and file not in exclude_files and file in include_files]  
+files = [file for file in os.listdir('data') if file.endswith(".xlsx") and file not in exclude_files and file in include_files]
 print(len(files))
 
 # window_bo = 100
