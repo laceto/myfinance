115e8edc92fd7296a9935bdd413ff8822b192ab5
Luigi Aceto <luigi.aceto@genre.com>
2024-07-23 14:39:50 +0200

    Update and rename get_insigths.py to plots.py

---
+++ plots.py
@@ -0,0 +1,37 @@
+import sys  
+import os  
+import pandas as pd  
+
+sys.path.insert(0, '..')
+from AT_funs import *
+
+
+filename_bm = 'FTSEMIB.MI.xlsx'
+bm_df = read_xlsx(filename_bm)
+bm_name = filename_bm.replace('xlsx', '')
+
+exclude_files = {"FTSEMIB.MI.xlsx", "marginabili.xlsx", "sectors.xlsx", "output_signals.xlsx", "PTF.xlsx"}
+include_files = {"A2A.MI.xlsx"}
+files = [file for file in os.listdir('/home/laceto/AT/screaning_data') if file.endswith(".xlsx") and file not in exclude_files and file in include_files]  
+print(len(files))
+
+def save_plot(df, ticker):
+    close_price = df[['close','rclose']].plot(figsize=(20,8),grid=True, title= ticker +  ' Absolute vs relative to ' + bm_name + ' rebased' )
+    plt.savefig('plots' + ticker + '.png') 
+    return df
+
+for i in range(len(dfs)):
+    print(i)
+    ohlc = ['open','high','low','close']
+    _o,_h,_l,_c = [ohlc[h] for h in range(len(ohlc))]
+
+    ticker = dfs[i]['ticker'][0]
+    try:
+        dfs[i] = relative(dfs[i],_o,_h,_l,_c, bm_df, bm_col, dgt, rebase=True)
+        dfs[i] = save_plot(dfs[i], ticker)
+
+
+    except:
+        failed.append(i)
+
+print('for signal done')
