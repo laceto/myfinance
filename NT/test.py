a5a09c414f59b2ece2e49904cc923474329c077c
Luigi Aceto <luigi.aceto@genre.com>
2025-01-01 18:06:44 +0100

    Update test.py

--- NT/test.py
+++ NT/test.py
@@ -3,14 +3,7 @@ import os
   
 # Add the 'data' directory to sys.path  
 sys.path.insert(0, 'data')  
-  
-# Get a list of all Python files in the 'data' directory  
-py_files = [f for f in os.listdir('data') if f.endswith('.py')]  
-  
-# Loop over the list of Python files and import each one  
-for f in py_files:  
-    module_name = os.path.splitext(f)[0]  # Extract the module name from the file name  
-    module = __import__(module_name)  # Import the module  
+from relative import * 
 
 sys.path.insert(0, '..')
 from AT_funs import *
