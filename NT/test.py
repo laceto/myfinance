0451b21efaf244d47eb1c859b5571044e6e0d973
Luigi Aceto <luigi.aceto@genre.com>
2025-01-01 17:31:44 +0100

    Update test.py

--- NT/test.py
+++ NT/test.py
@@ -1,6 +1,10 @@
-import sys  
+#import sys  
+#sys.path.insert(0, '..')
+#from AT_funs import *
+
+#sys.path.insert(0, 'data')
+#from relative import *
+
+from data import relative
 sys.path.insert(0, '..')
 from AT_funs import *
-
-sys.path.insert(0, 'data')
-from relative import *
