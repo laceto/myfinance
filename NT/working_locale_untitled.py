9cab1cdb6c767042baa0f73bcd6c2aea673a3320
luigi.aceto <luigi.aceto@genre.com>
2025-01-08 08:07:20 +0100

    new template

---
+++ NT/working_locale_untitled.py
@@ -0,0 +1,53 @@
+# for one file at time
+# import papermill as pm
+# import os
+  
+# # Set the name of your notebook  
+# notebook_name = 'template.ipynb'  
+
+# # Set the path to your file  
+# path_to_file = 'data/A2A.MI.xlsx'
+
+# # Define the parameters to pass to the notebook  
+# parameters = {'path_to_file': path_to_file}
+
+# # Set the path for the output notebook  
+# output_notebook_name = os.path.basename(path_to_file).replace('.xlsx', '') + '.ipynb'
+  
+# # Execute the notebook using subprocess  
+# # subprocess.run(['jupyter', 'nbconvert', notebook_name, '--to', 'html', '--execute']) 
+# pm.execute_notebook(notebook_name, output_notebook_name, parameters=parameters)  
+
+
+
+import os  
+import papermill as pm  
+  
+# Set the name of your notebook  
+notebook_name = 'template.ipynb'  
+os.chdir("..")   
+# Set the path to the folder containing the files  
+folder_path = 'data/'  
+  
+# Get a list of the first 3 files in the folder  
+file_list = os.listdir(folder_path)
+
+os.chdir('NT')
+
+for file_name in file_list:  
+    # Set the path to the file  
+    path_to_file = os.path.join(folder_path, file_name)  
+  
+    # Define the parameters to pass to the notebook  
+    parameters = {'path_to_file': path_to_file}  
+  
+    # Set the path for the output notebook  
+    output_notebook_name = os.path.splitext(file_name)[0] + '_output.ipynb'  
+  
+    # # Execute the notebook using Papermill and save the output to a new notebook  
+    # pm.execute_notebook(notebook_name, output_notebook_name, parameters=parameters) 
+
+    # Ignore the .ipynb_checkpoints folder  
+    if not file_name.startswith('.'):  
+        # Execute the notebook using Papermill and save the output to a new notebook  
+        pm.execute_notebook(notebook_name, output_notebook_name, parameters=parameters)  
