315e479d12ae616969fde10d1884892eb785a825
luigi.aceto <luigi.aceto@genre.com>
2025-02-09 16:09:13 +0100

    add nasdaq ticker

--- new tickers.R
+++ new tickers.R
@@ -2,7 +2,7 @@
 
 tickers <- readRDS("tickers.rds")
 
-new_tickers <- c('SOUN', 'GTLB', 'SNOW', 'RGTI', 'APP', 'S', 'RKLB', 'PLTR', 'AI', 'PANW', 'IONQ', 'TEM')
+new_tickers <- c('SOUN', 'GTLB', 'SNOW', 'RGTI', 'APP', 'S', 'RKLB', 'PLTR', 'AI', 'PANW', 'IONQ', 'TEM', 'NDX')
 
 tickers <- c(tickers, new_tickers)
 
