bfbd7e0225358e97c87cf0b1b7d1d26bc11798ab
luigi.aceto <luigi.aceto@genre.com>
2025-02-09 16:10:41 +0100

    add nasdaq ticker

--- data/test.py
+++ data/test.py
@@ -13,7 +13,7 @@ bm_name = filename_bm.replace('xlsx', '')
 # files = [file for file in os.listdir('data') if file.endswith(".xlsx") and file != "FTSEMIB.MI.xlsx" and file != "marginabili.xlsx" and file != "sectors.xlsx" and file != "output_signals.xlsx" and file != "PTF.xlsx", and file == "A2A.MI.xlsx"]
 # print(len(files))
 
-exclude_files = {"FTSEMIB.MI.xlsx", "marginabili.xlsx", "sectors.xlsx", "output_signals.xlsx", "PTF.xlsx"}
+exclude_files = {"FTSEMIB.MI.xlsx", "marginabili.xlsx", "sectors.xlsx", "output_signals.xlsx", "PTF.xlsx", "NDX.xlsx"}
 # include_files = {"A2A.MI.xlsx"}
 # files = [file for file in os.listdir('data') if file.endswith(".xlsx") and file not in exclude_files and file in include_files]
 files = [file for file in os.listdir('data') if file.endswith(".xlsx") and file not in exclude_files]
