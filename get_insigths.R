3660a0e9be354b6a8cb1e1da2c31004ec09bdb1a
Luigi Aceto <l_aceto@hotmail.it>
2025-02-19 21:45:45 +0100

    Update get_insigths.R

--- get_insigths.R
+++ get_insigths.R
@@ -1166,10 +1166,11 @@ rh3 <- output_signal %>%
       dplyr::group_by(ticker) %>% 
       dplyr::slice_head(n = 1)
   ) %>% 
-  dplyr::filter(change == -1, RSI < 50, ticker_score <= 0) 
+  dplyr::filter(change == -1, RSI < 50) 
 
 
 rh3 %>% 
   dplyr::left_join(readr::read_delim('signals/sector_ticker.txt')) %>% 
+  dplyr::filter(ticker_score <=0) %>%
   dplyr::select(-c(swing, method, change, sector, last_day_score, marginabile)) %>%
   write.table("signals/bear_high3_rsi_score.txt", sep = "\t", dec = ".", row.names = FALSE)
